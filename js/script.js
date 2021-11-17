4ad8d2c29b8a11046b79e1c06be8cdc57955a5d6
William Finocchiaro <89465648+WILLIAMFINOCCHIARO@users.noreply.github.com>
2021-11-17 16:59:11 +0100

    initial

--- js/script.js
+++ js/script.js
@@ -5,7 +5,8 @@
 // con difficoltà 2 => tra 1 e 81
 // con difficoltà 3 => tra 1 e 49
 // Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro.
-// Consigli del giorno: :party_wizard: ****Scriviamo prima cosa vogliamo fare passo passo in italiano, dividiamo il lavoro in micro problemi. Ad esempio:
+// Consigli del giorno: :party_wizard: ****Scriviamo prima cosa vogliamo fare passo passo in italiano,
+// dividiamo il lavoro in micro problemi. Ad esempio:
 // Di cosa ho bisogno per generare i numeri?
 // Proviamo sempre prima con dei console.log() per capire se stiamo ricevendo i dati giusti.
 // Le validazioni e i controlli possiamo farli anche in un secondo momento.
